5261e4cca560d226cc0448728510453227ee1d21
hasan <hasanraja2199@gmail.com>
2020-12-11 19:52:59 +0530

    sidebar created

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hasan raja twitter clone</h1>
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* feed */}
+
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
